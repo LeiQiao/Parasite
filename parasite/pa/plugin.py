8e9ef0b8c66c8c131209d533e39473eff29043f7
lei.qiao <lei.qiao@chinapnr>
2019-08-22 15:46:38 +0800

    添加插件路径 添加卸载插件的装饰器

--- parasite/pa/plugin.py
+++ parasite/pa/plugin.py
@@ -1,10 +1,12 @@
 class Plugin:
     before_load_methods = []
     after_load_methods = []
+    before_unload_methods = []
 
-    def __init__(self, plugin_name=None, manifest=None):
+    def __init__(self, plugin_name=None, manifest=None, plugin_path=None):
         self.plugin_name = plugin_name
         self.manifest = manifest
+        self.plugin_path = plugin_path
 
     @staticmethod
     def before_load(f):
@@ -16,12 +18,21 @@ class Plugin:
         if f not in Plugin.after_load_methods:
             Plugin.after_load_methods.append(f)
 
+    @staticmethod
+    def before_unload(f):
+        if f not in Plugin.before_unload_methods:
+            Plugin.before_unload_methods.append(f)
+
     def on_before_load(self):
         for f in Plugin.before_load_methods:
             f(self)
 
     def on_after_load(self):
         for f in Plugin.after_load_methods:
+            f(self)
+
+    def on_before_unload(self):
+        for f in Plugin.before_unload_methods:
             f(self)
 
     # Overridable -- handle load plugin event
