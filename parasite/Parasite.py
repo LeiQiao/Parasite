8343ccb659091922c1613986ff64aa649e545b96
lei.qiao <lei.qiao@chinapnr>
2020-05-14 14:35:18 +0800

    删除 flask ，将 flask 服务作为插件加入

--- parasite/Parasite.py
+++ parasite/Parasite.py
@@ -1,13 +1,8 @@
-from flask import Flask
 import pa
 from plugins.base import *
-
-
-app = Flask(__name__)
-pa.web_app = app
 
 # 默认加载 base 插件
 BasePlugin().on_load()
 
-if __name__ == '__main__':
-    app.run(host=pa.server_ip, port=int(pa.server_port), threaded=True)
+if True:
+    pa.web_app.run(host=pa.server_ip, port=int(pa.server_port), threaded=True)
