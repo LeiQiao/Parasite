5659a3b6c608555973e88328aa661ccf94b064aa
lei.qiao <lei.qiao@chinapnr>
2020-06-10 14:48:24 +0800

    增加直接打包的功能

--- parasite/pa/bin/deploy_sh.py
+++ parasite/pa/bin/deploy_sh.py
@@ -1,6 +1,9 @@
 import os
 from .download_source import pa_root, parasite_git_url, get_all_depend_manifest
 from .download_source import get_plugin_manifest_path as __get_plugin_manifest_path
+import ast
+import git
+import shutil
 
 
 def get_plugin_manifest_path(plugin_name):
@@ -143,3 +146,179 @@ def deploy_sh(project_name, manifest_file, config_file=None,
     # 保存
     with open(os.path.join(output_dir, output_name), 'w') as f:
         f.write(sh_content)
+
+
+def package_tar(project_name, manifest_file, config_file=None,
+                extra_plugin_paths=None, resource_file=None, tar_name=None):
+    all_manifest = get_all_depend_manifest(pa_root, manifest_file, extra_plugin_paths)
+
+    # 创建换从工作文件夹
+    if not os.path.exists('.pa.cache'):
+        os.makedirs('.pa.cache', exist_ok=True)
+
+    if not os.path.exists('.pa.cache/gits'):
+        os.makedirs('.pa.cache/gits', exist_ok=True)
+
+    if not os.path.exists('.pa.cache/gits/Parasite'):
+        print('\033[34m downloading Parasite... \033[0m')
+        git.Repo.clone_from(url=parasite_git_url, to_path='.pa.cache/gits/Parasite')
+    else:
+        print('\033[34m updating Parasite... \033[0m')
+        g = git.cmd.Git('.pa.cache/gits/Parasite')
+        g.reset('--hard')
+        g.pull()
+
+    # 开始打包
+    project_path = os.path.join('.pa.cache', project_name)
+    project_plugin_path = os.path.join(project_path, 'plugins')
+    if os.path.exists(project_path):
+        shutil.rmtree(project_path, ignore_errors=True)
+
+    shutil.copytree('.pa.cache/gits/Parasite/parasite', project_path)
+
+    # 复制配置文件
+    if config_file is not None:
+        print('\033[34m writing \'config.conf\'... \033[0m')
+        shutil.copyfile(config_file, os.path.join(project_path, 'config.conf'))
+
+    # 资源文件
+    if resource_file is not None:
+        index = 1
+        resource_length = len(resource_file.keys())
+        for file, dest_path in resource_file.items():
+            origin_file_name = os.path.basename(file)
+            file_names = []
+            if isinstance(dest_path, list):
+                file_names = dest_path
+            else:
+                file_names.append(dest_path)
+
+            for file_name in file_names:
+                if len(file_name) > 0:
+                    if os.path.isdir(file_name) or os.path.basename(file_name) == '':
+                        file_name = os.path.join(file_name, origin_file_name)
+                else:
+                    file_name = origin_file_name
+
+                if file_name[0] == '/':
+                    file_name = file_name[1:]
+
+                print("\033[34m writing resource file \'{0}\'... \033[0m".format(
+                    file_name, index, resource_length))
+
+                file_name = os.path.join(project_path, file_name)
+                dest_path = os.path.dirname(file_name)
+
+                if not os.path.exists(dest_path):
+                    os.makedirs(dest_path, exist_ok=True)
+                shutil.copyfile(file, file_name)
+
+    cached_gits = {}
+    for d in os.listdir('.pa.cache/gits'):
+        # if d == 'Parasite':
+        #     continue
+        git_path = os.path.join('.pa.cache/gits', d)
+        g = git.cmd.Git(git_path)
+        url = g.config('remote.origin.url')
+        branch = g.rev_parse('--abbrev-ref', 'HEAD')
+        cached_gits[url] = {
+            'branch': branch,
+            'path': git_path,
+            'pulled': False
+        }
+
+    for plugin_name, manifest in all_manifest.items():
+        if 'git' in manifest['source']:
+            git_address = manifest['source']['git']
+            if len(git_address) == 0:
+                raise ModuleNotFoundError('plugin {0} donot have an git repository.'.format(manifest['name']))
+            git_branch = manifest['source']['branch'] if 'branch' in manifest['source'] else 'master'
+            # 设置子目录
+            if 'path' in manifest['source'] and len(manifest['source']['path']) > 0:
+                git_source_path = os.path.join(manifest['source']['path'], plugin_name)
+            else:
+                git_source_path = plugin_name
+        else:
+            raise NotImplementedError('only support \'git\' for revision control tool.')
+
+        src_plugin_path = ''
+        for extra_path in extra_plugin_paths:
+            plugin_paths = os.listdir(extra_path)
+            for plugin_path in plugin_paths:
+                try:
+                    with open(os.path.join(extra_path, plugin_path, '__manifest__.py')) as f:
+                        m2 = ast.literal_eval(f.read())
+                except Exception as e:
+                    _ = e
+                    continue
+
+                if m2['name'] == manifest['name'] and m2['version'] == manifest['version']:
+                    src_plugin_path = os.path.join(extra_path, plugin_path)
+                    break
+            if len(src_plugin_path) > 0:
+                break
+
+        if len(src_plugin_path) == 0:
+            for git_url, cached_git in cached_gits.items():
+                if git_url != git_address:
+                    continue
+                g = git.cmd.Git(cached_git['path'])
+                if git_branch != cached_git['branch']:
+                    g.check_out(git_branch)
+                    cached_git['branch'] = git_branch
+                if not cached_git['pulled']:
+                    print('\033[34m updating {0}... \033[0m'.format(os.path.basename(git_url)))
+                    g.reset('--hard')
+                    g.pull()
+                    cached_git['pulled'] = True
+                src_plugin_path = os.path.join(cached_git['path'], git_source_path)
+                break
+
+        if len(src_plugin_path) == 0:
+            print('\033[34m downloading {0} from {1}... \033[0m'.format(plugin_name, git_address))
+            git_path = os.path.join('.pa.cache/gits', os.path.basename(git_address))
+            git.Repo.clone_from(url=git_address, to_path=git_path, branch=git_branch)
+
+            cached_gits[git_address] = {
+                'branch': git_branch,
+                'path': git_path,
+                'pulled': True
+            }
+
+            src_plugin_path = os.path.join(git_path, git_source_path)
+
+        if len(src_plugin_path) == 0:
+            raise FileNotFoundError('unable found plugin {0}'.format(plugin_name))
+
+        print('\033[34m installing plugin \'{0}\'... \033[0m'.format(plugin_name))
+        shutil.copytree(src_plugin_path, os.path.join(project_plugin_path, plugin_name))
+
+    # 打包
+    output_name = '{0}.tar.bz2'.format(project_name)
+    if tar_name is not None and len(tar_name) > 0:
+        if os.path.isdir(tar_name) or os.path.basename(tar_name) == '':
+            output_dir = tar_name
+        else:
+            output_dir = os.path.dirname(tar_name)
+            if len(output_dir) == 0:
+                output_dir = './'
+            output_name = os.path.basename(tar_name)
+    else:
+        output_dir = os.getcwd()
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    print('\033[34m packaging {0}... \033[0m'.format(output_name))
+    if output_name is not None:
+        if output_name[-8:].lower() != '.tar.bz2':
+            output_name = output_name + '.tar.bz2'
+    os.system("cd .pa.cache &&tar cJf {0} {1} && cd ..".format(
+        output_name,
+        project_name))
+    shutil.move(os.path.join('.pa.cache', output_name), os.path.join(output_dir, output_name))
+
+    # clean up
+    shutil.rmtree(project_path, ignore_errors=True)
+
+    print('\033[34m done \033[0m')
